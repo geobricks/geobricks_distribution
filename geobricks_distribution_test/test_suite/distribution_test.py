8840d4362c502501e48dfdebded2608042451001
simonefenix <simone.murzilli@fao.org>
2015-03-02 16:23:18 +0100

    enabled test

--- geobricks_distribution_test/test_suite/distribution_test.py
+++ geobricks_distribution_test/test_suite/distribution_test.py
@@ -94,42 +94,41 @@ class GeobricksTest(unittest.TestCase):
     distribution = Distribution(config)
 
     # Raster
-    # def test_distribution_raster(self):
-    #     result = self.distribution.export_raster_by_spatial_query(json_request_export_raster)
-    #     self.assertEqual(os.path.isfile(result), True)
+    def test_distribution_raster(self):
+        result = self.distribution.export_raster_by_spatial_query(json_request_export_raster)
+        self.assertEqual(os.path.isfile(result), True)
 
-    # def test_distribution_raster_spatialquery_rest(self):
-    #     try:
-    #         requests.get("http://localhost:5904/distribution/discovery")
-    #     except Exception:
-    #         log.warn("Service is down. Please run rest/distribution_main.py to run the test")
-    #     headers = {'content-type': 'application/json'}
-    #     data = simplejson.dumps(json_request_export_raster)
-    #     r = requests.post("http://localhost:5904/distribution/raster/spatialquery/", data=data, headers=headers)
-    #     self.assertEqual(200, r.status_code)
-    #
-    # def test_distribution_download_rest(self):
-    #     try:
-    #         requests.get("http://localhost:5904/distribution/discovery")
-    #     except Exception, e:
-    #         log.warn("Service is down. Please run rest/distribution_main.py to run the test")
-    #
-    #     headers = {'content-type': 'application/json'}
-    #     data = simplejson.dumps(json_request_export_raster)
-    #     r = requests.post("http://localhost:5904/distribution/raster/spatialquery/", data=data, headers=headers)
-    #     uid = simplejson.loads(simplejson.loads(r.text))
-    #     r = requests.get(uid["url"])
-    #     self.assertEqual(200, r.status_code)
+    def test_distribution_raster_spatialquery_rest(self):
+        try:
+            requests.get("http://localhost:5904/distribution/discovery")
+        except Exception:
+            log.warn("Service is down. Please run rest/distribution_main.py to run the test")
+        headers = {'content-type': 'application/json'}
+        data = simplejson.dumps(json_request_export_raster)
+        r = requests.post("http://localhost:5904/distribution/raster/spatialquery/", data=data, headers=headers)
+        self.assertEqual(200, r.status_code)
+
+    def test_distribution_download_rest(self):
+        try:
+            requests.get("http://localhost:5904/distribution/discovery")
+        except Exception, e:
+            log.warn("Service is down. Please run rest/distribution_main.py to run the test")
+        headers = {'content-type': 'application/json'}
+        data = simplejson.dumps(json_request_export_raster)
+        r = requests.post("http://localhost:5904/distribution/raster/spatialquery/", data=data, headers=headers)
+        uid = simplejson.loads(simplejson.loads(r.text))
+        r = requests.get(uid["url"])
+        self.assertEqual(200, r.status_code)
 
 
     # Vector
-    # def test_distribution_vector(self):
-    #     result = self.distribution.export_vector_by_spatial_query(json_request_export_vector)
-    #     self.assertEqual(os.path.isfile(result), True)
-    #
-    # def test_distribution_vector_different_prj(self):
-    #     result = self.distribution.export_vector_by_spatial_query(json_request_export_vector_different_prj)
-    #     self.assertEqual(os.path.isfile(result), True)
+    def test_distribution_vector(self):
+        result = self.distribution.export_vector_by_spatial_query(json_request_export_vector)
+        self.assertEqual(os.path.isfile(result), True)
+
+    def test_distribution_vector_different_prj(self):
+        result = self.distribution.export_vector_by_spatial_query(json_request_export_vector_different_prj)
+        self.assertEqual(os.path.isfile(result), True)
 
     def test_distribution_vector_spatialquery_rest(self):
         try:
@@ -140,7 +139,6 @@ class GeobricksTest(unittest.TestCase):
         data = simplejson.dumps(json_request_export_vector)
         r = requests.post("http://localhost:5904/distribution/shp/spatialquery/", data=data, headers=headers)
         self.assertEqual(200, r.status_code)
-
 
 
 def run_test():
